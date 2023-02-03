aec9abdfb7afdab64231218d31af5c06760c39ba
dg4897 <guleriad797@gmail.com>
2023-02-03 11:19:25 +0530

    commit message

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const router=require('./routes/index')
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
